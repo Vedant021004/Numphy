9264dc418ff737cd5e5655274237ea7cc6815faa
Vedant021004 <vedantkp79@gmail.com>
2026-05-27 15:27:14 +0530

    completed 25 questions

--- numpy_practice.py/INDEXING_SLICING🔥.py
+++ numpy_practice.py/INDEXING_SLICING🔥.py
@@ -8,3 +8,28 @@ print(arr[:])
 print(arr[-1])
 print(arr[1:])
 
+# Print elements from index 2 to 6
+arr1 = np.array([1,2,3,4,5,6,7,8,9,10])
+print(arr1[2:6])
+
+# Reverse an array.
+print(arr1[::-1])
+
+# Print alternate elements
+print(arr1[::2])
+print(arr[1::2])
+
+# Create 3×3 matrix and print second row.
+arr2 = np.array([[1,2,4],[5,6,7],[9,8,0]])
+print(arr2)
+print(arr2[0, :])
+print(arr2[1, :])
+print(arr2[2, :])
+
+# Print third column from matrix.
+print(arr2[:, 0])
+print(arr2[:, 1])
+print(arr2[:, 2])
+
+arr2[0,0] =100
+print(arr2)
