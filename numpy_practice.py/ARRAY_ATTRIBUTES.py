c438229b72d8d5a969160a037f79c663bb70c856
Vedant021004 <vedantkp79@gmail.com>
2026-05-27 14:10:57 +0530

    done

--- numpy_practice.py/ARRAY_ATTRIBUTES.py
+++ numpy_practice.py/ARRAY_ATTRIBUTES.py
@@ -9,8 +9,15 @@ print(arr11.shape)
 # Find dimensions of array.
 mwaah = np.array([[[1,2,4],[1,2,3],[1,2,3]]])
 print(mwaah)
-print(mwaah.ndim)
-print(mwaah.shape)
-print(mwaah.size)
-print(mwaah.itemsize)
-print(mwaah.nbytes)
+
+print(mwaah.ndim)  #dimemsions
+
+print(mwaah.shape)  #shape of the array
+
+print(mwaah.size)  #total size of hte array 
+
+
+print(mwaah.itemsize)  #per byte of an element
+
+
+print(mwaah.nbytes)  # total data store by teh array
