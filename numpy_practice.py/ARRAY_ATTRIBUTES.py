8f22ebd270491b750881a61ff5d4419ea1591486
Vedant021004 <vedantkp79@gmail.com>
2026-05-27 13:58:55 +0530

    array attributes

---
+++ numpy_practice.py/ARRAY_ATTRIBUTES.py
@@ -0,0 +1,10 @@
+# Find shape of array.
+import numpy as np
+arr1 = np.array([1,2,3,4])
+print(arr1.shape)
+
+arr11 = np.array([[1,2,3],[5,6,7]])
+print(arr11.shape)
+
+# Find dimensions of array.
+mwaah
